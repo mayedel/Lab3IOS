96200331cd64d15f3769227ec90e7538ca72b549
María Espejo <mayed92@hotmail.com>
2024-05-06 13:45:53 +0200

    Initial Commit

---
+++ Lab3/Lab3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3App.swift
+//  Lab3
+//
+//  Created by María Espejo on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
